08251b7f2d8750fe360c1dc6a85769b926126974
bchavs12 <chavesbruno699@gmail.com>
2023-09-09 19:47:48 -0300

    add Read-me

---
+++ Promises.js
@@ -0,0 +1,7 @@
+/**
+ * Uma Promise é um objeto que representa a eventual conclusão ou falha de uma operação assíncrona.
+ * Ela pode estar em um dos três estados: 
+ * - pendente (pending), 
+ * - cumprida (fulfilled),
+ * - rejeitada (rejected). 
+ */
